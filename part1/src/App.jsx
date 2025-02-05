7cb9aa996251d7dd66623f4caf5d897be6d0bf31
David <estebandav23@gmail.com>
2025-02-05 18:20:36 -0500

    af

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,87 +1,12 @@
-const Part = (props) => {
-
-  return (
-    <div>
-      <p>
-       {props.name}, {props.exercises}
-      </p>
-    </div>
-  )
-}
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  const sum = (p1, p2) => {
+    console.log(p1)
+    console.log(p2)
+    return p1 + p2
   }
 
-
-
-
-  let exer1 = 0
-  let exername
-  course.parts.map(value => exer1 += value.exercises)
-
-  course.parts.map(value => exername = value.name)
-  
-  const Header = () => {
-    return (
-    <div>
-    
-    {course.name}
-    
-    </div>
-    )
-  }
-  
-  const Total = () => {
-
-    
-
-    return (
-    <div>
-    
-           <p>Total exercises is {exer1} </p>
-    
-    </div>
-    )
-  }
-  
-  const Content = () => {
-    
-    return (
-    
-    <div>
-      {
-        course.parts.map(value => <Part name={ value.name } exercises={ value.exercises }/>)
-      }
-    </div>
-    
-    )
-  }
-
-
-  return (
-    <div>
-      <Header course={course} />
-      <Content parts={course} />
-      <Total parts={course} />
-    </div>
-  )
-  
-    }
-
+  const result = sum(1, 5)
+console.log(result)
+}
 export default App
