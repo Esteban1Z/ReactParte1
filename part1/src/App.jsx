e5a2816cf221f7b76bbf20be03524f7691d4cdaf
David <estebandav23@gmail.com>
2025-02-13 10:08:42 -0500

    1.9 clear

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,119 +1,71 @@
 import { useState } from 'react'
-import ReactDOM from "react-dom";
 
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
 
-const StatisticLine = (props) => {
+  </button>
+)
 
-  return(
+const Statistics = (props) => {
+    if (props.good === 0 && props.neutral === 0 && props.bad === 0) {
+      return (
+        <div>
+          <h2>Statistics</h2>
+          <p>No feedback given</p>
+        </div>
+      )
+    }else{
+      return (
     <div>
-      <p>{props.text} {props.value}</p>
-      
+      <h2>Statistics</h2>
+      <p>
+      good {props.good}<br/>
+      neutral {props.neutral}<br/>
+      bad {props.bad}<br/>
+      average {props.average}<br/>
+      positive {props.positive}%
+      </p>
     </div>
   )
-
+}
 }
 
-const Statistics = ({ good, neutral, bad, all, average, positiveResult}) => {
-
-
-    return( 
-      
-        <table>
-        <tbody>
-        <tr>
-        <td>
-        <div>
-        <StatisticLine text="good" />                                       <StatisticLine value={good}/>
-        <StatisticLine text="neutral" />                     <StatisticLine value={neutral}/>
-        <StatisticLine text="bad" />                             <StatisticLine value={bad}/>
-        <StatisticLine text="all" />               <StatisticLine value={all}/>
-        <StatisticLine text="average" />                         <StatisticLine value={average}/>
-        <StatisticLine text="positive" />             <StatisticLine value={positiveResult}/>
-        </div>
-        </td>
-        </tr>
-        </tbody>
-        
-        </table>
-      
-    )
-  }
-
-  
 const App = () => {
-
+  // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(0)
 
-  const setToGood = () => {
-    console.log('good', good)
-    setAll(good + neutral + bad)
-    setGood(good + 1)
+  const average = (good - bad) / (good + bad)
+  const positive = good / (good + neutral + bad) * 100
+
+  const goodCont = () => {
+    setGood(good+1)
+    console.log('good after', good)
   }
 
-  const setToNeutral = () => {
-    console.log('neutral', neutral)
-    setAll(good + neutral + bad)
-    setNeutral(neutral + 1)
+  const neutralCont = () => {
+    setNeutral(neutral+1)
+    console.log('neutral after', neutral)
   }
 
-  const setToBad = () => {
-    console.log('bad', bad)
-    setAll(good + neutral + bad)
-    setBad(bad + 1)
+  const badCont = () => {
+    setBad(bad+1)
+    console.log('bad after', bad)
   }
-
-  const Button = (props) => (
-    <button onClick={props.handleClick}>
-    {props.text}
-    </button>
-  )
-
-  const positiveResult = good / all * 100;
-
-  const average = good / all;
-
-  const History = () => {
-    if ((good === 0) && (neutral === 0) && (bad === 0)) {
-      return (
-        <div>
-          No feedback given
-        </div>
-      )
-      
-    }
-    return (
-      <div>
-        <Statistics 
-        good={good}
-        neutral={neutral}
-        bad={bad}
-        all={all}
-        average={average}
-        positiveResult={positiveResult}/>
-      </div>
-    )
-  }
-
 
   return (
     <div>
+      <h1>Give Feedback</h1>
 
-      <h1>give feedback</h1>
-      <Button handleClick={() => setToGood(good + 1)} text="Good" />
-      <Button handleClick={() => setToNeutral(neutral + 1)} text="Neutral" />
-      <Button handleClick={() => setToBad(bad + 1)} text="Bad" />
-      <h1>statistics</h1>
-      <History good={good}/>
-    </div>
+      <Button onClick={goodCont} text='good' />
+      <Button onClick={neutralCont} text='neutral' />
+      <Button onClick={badCont} text='bad' />
+
+      <Statistics good={good} neutral={neutral} bad={bad} average={average} positive={positive}/>
+      </div>
   )
-
-
 }
-
-
-
 
 export default App
