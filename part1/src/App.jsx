243f4e4151e54e5b758822b9587e8a4cd5d30446
David <estebandav23@gmail.com>
2025-02-15 14:29:04 -0500

    1.14 clear

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,84 +1,45 @@
 import { useState } from 'react'
 
-const Button = ({ onClick, text }) => (
-  <button onClick={onClick}>
-    {text}
-
-  </button>
-)
-
-const Statistics = (props) => {
-    if (props.good === 0 && props.neutral === 0 && props.bad === 0) {
-      return (
-        <div>
-          <h2>Statistics</h2>
-          <p>No feedback given</p>
-        </div>
-      )
-    }else{
-      return (
-    <div>
-      <h2>Statistics</h2>
-      <table>
-      <tbody>
-      <StatisticLine text="good" value ={props.good} />
-      <StatisticLine text="neutral" value ={props.neutral} />
-      <StatisticLine text="bad" value ={props.bad} />
-      <StatisticLine text="all" value ={props.all} />
-      </tbody>
-      </table>
-
-    </div>
-  )
-}
-}
-
-const StatisticLine = ({ text, value }) => {
-
-  return (
-    <div>
-      {text} {value}
-    </div>
-  )
-
-}
-
 const App = () => {
-  // guarda los clics de cada botón en su propio estado
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
 
-  const average = (good - bad) / (good + bad)
-  const positive = good / (good + neutral + bad) * 100
-  const all = good + neutral + bad
+  const [selected, setSelected] = useState(0)
+  const [votesNum, setVotes] = useState(new Array(anecdotes.length).fill(0))
 
-  const goodCont = () => {
-    setGood(good+1)
-    console.log('good after', good)
+  
+  const copy = () => {
+    const newVotes = [...votesNum]
+    newVotes[selected] += 1
+    setVotes(newVotes)
   }
 
-  const neutralCont = () => {
-    setNeutral(neutral+1)
-    console.log('neutral after', neutral)
-  }
+  const Aleatorio = () => {
+    const indiceAleatorio = Math.floor(Math.random() * anecdotes.length);
+    setSelected(indiceAleatorio);
+  };
 
-  const badCont = () => {
-    setBad(bad+1)
-    console.log('bad after', bad)
-  }
-
+  const maxVotes = Math.max(...votesNum);
+  const maxVotesIndex = votesNum.indexOf(maxVotes);
+  const anecdoteWithMostVotes = anecdotes[maxVotesIndex];
 
   return (
     <div>
-      <h1>Give Feedback</h1>
-
-      <Button onClick={goodCont} text='good' />
-      <Button onClick={neutralCont} text='neutral' />
-      <Button onClick={badCont} text='bad' />
-
-      <Statistics good={good} neutral={neutral} bad={bad} average={average} positive={positive} all={all}/>
-      </div>
+    <button onClick={Aleatorio}>anecdota</button>
+    <button onClick={copy}>votos</button>
+    <p>{anecdotes[selected]}</p>
+    <p>{votesNum[selected]} votes</p>
+    <p>{anecdoteWithMostVotes}</p>
+    <p>{maxVotes}</p>
+    </div>
   )
 }
 
